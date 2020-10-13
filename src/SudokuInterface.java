96bffc12a678172c3eeb6b32d7c13ca59ce21c31
J.D. Holwick <70964794+jdholwick@users.noreply.github.com>
2020-10-13 10:42:25 -0600

    CSCI 391 - MVC Sukoku Project

--- src/SudokuInterface.java
+++ src/SudokuInterface.java
@@ -8,13 +8,13 @@ public class SudokuInterface {
 
     static void mainMenu(int iMenuOpt) { // will produce the proper menu options in the moment
         int menuChoice;
-        Scanner menuInpt = new Scanner(System.in);
+        Scanner menuInput = new Scanner(System.in);
 
 
         if (iMenuOpt == 1) {
             System.out.println("Welcome to a simple Sudoku game interface!");
             System.out.println("\n\nPlease select from the following options:\n   1) Start a game\n   2) Exit\n\n");
-            menuChoice = menuInpt.nextInt();
+            menuChoice = menuInput.nextInt();
         }
         else if (iMenuOpt == 2) {
             System.out.println("2...");
