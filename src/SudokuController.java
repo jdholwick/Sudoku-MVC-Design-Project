23ccb1dbf8247d4b2e40f44e18612282d6c42a54
J.D. Holwick <70964794+jdholwick@users.noreply.github.com>
2020-10-13 16:29:34 -0600

    CSCI 391 - MVC Sukoku Project

--- src/SudokuController.java
+++ src/SudokuController.java
@@ -6,9 +6,11 @@ public class SudokuController {
 
     public static void initNewGame() { // will initiate a new game
         SudokuInterface theInterf = new SudokuInterface();
+        SudokuModel theModel = new SudokuModel();
 
         if (theInterf.mainMenu(1) == 1) {
             System.out.println("will call Game Board class...");
+            theModel.newBoard();
         }
     }
 
--- src/SudokuModel.java
+++ src/SudokuModel.java
@@ -17,27 +17,52 @@ public class SudokuModel {
         // so for now since the concern is more the MVC design, i am using one simple
         // preloaded set of numbers to put into iGameSquares
 
-        Random rand7 = new Random(); // random variable that will choose between 1 and 10 to decide if a a number should be hidden or not
-        int [] i7Count = new int[9];
+        Random rand6 = new Random(); // will be used to select 6 from each column to turn to 0
+        int [] i6Count = new int[6]; // will hold seven of integers 1-9 and so each i6Count[k] will represent the j index such that iGameSquare[i][i6Count[k]] == 0
 
         for (int i = 0; i < 9; i++) { // cycle through each column
-            for (int k = 0; k < 7; k++) { // k index is for i7Count[k] (which itself will be j index) so we can hold seven non repeating numbers 1-9
-                boolean bDup = true; // true initially and then only true when there is a repeating index number being held in i7Count
-                while (bDup) { // loops until a non duplicate number for i7Count[k] is found
+            for (int k = 0; k < 6; k++) { // k index is for i6Count[k] (which itself will be j index) so we can hold seven non repeating numbers 1-9
+                boolean bDup = true; // true initially and then only true when there is a repeating index number being held in i6Count
+                while (bDup) { // loops until a non duplicate number for i6Count[k] is found
                     bDup = false; // now set to false to begin test to see if a duplicate is found
-                    i7Count[k] = rand7.nextInt(9) + 1; // random number 1-9
-                    for (int l = 0; l < k; l++) { // if l == k that is comparing i7Count[l] to itself so l<k
-                        if (i7Count[l] == i7Count[k]) {
+                    i6Count[k] = rand6.nextInt(9); // random number 0-8
+                    for (int l = 0; l < k; l++) { // if l == k that is comparing i6Count[l] to itself so l<k
+                        if (i6Count[l] == i6Count[k]) {
                             bDup = true;
                             l = k; // to break for loop
                         }
                     }
                 }
-                iGameSquares[i][i7Count[k]] = 0; // sets j index to i7Count[k] which is one of 7 random non repeating numbers 1-9
+                iGameSquares[i][i6Count[k]] = 0; // sets j index to i6Count[k] which is one of 7 random non repeating numbers 1-9
             }
 
         }
 
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (iGameSquares[i][j] != 0) {
+                    if ((j == 2) || (j == 5)) {
+                        System.out.print(iGameSquares[i][j] + " |"); // prints fraction of vertical border after 3rd and 6th columns
+                    }
+                    else {
+                        System.out.print(iGameSquares[i][j] + " ");
+                    }
+                }
+                else {
+                    if ((j == 2) || (j == 5)) {
+                        System.out.print("- |"); // prints fraction of vertical border after 3rd and 6th columns
+                    }
+                    else {
+                        System.out.print("- ");
+                    }
+
+                }
+            }
+            System.out.println("");
+            if ((i == 2) || (i == 5)) {
+                System.out.println("____________________"); // prints horizontal border after 3rd and 6th rows
+            }
+        }
     }
 
     public void randNumLayout() {
